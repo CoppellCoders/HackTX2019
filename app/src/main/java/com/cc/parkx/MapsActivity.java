03ad0fe304cf7667fb016265809ab4fdc2d8ff02
Darshan Bhatta <bhatta.darshan26@gmail.com>
2019-11-03 02:10:00 -0600

    markers on places

--- app/src/main/java/com/cc/parkx/MapsActivity.java
+++ app/src/main/java/com/cc/parkx/MapsActivity.java
@@ -13,15 +13,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -29,13 +45,15 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap map;
     private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient fusedLocationClient;
+    final JSONArray array = new JSONArray();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_maps, container, false);
         String apiKey = getString(R.string.google_api_key);
         super.onCreate(savedInstanceState);
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         Places.initialize(getContext(), apiKey);
         PlacesClient placesClient = Places.createClient(getContext());
 
@@ -47,10 +65,41 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onPlaceSelected(Place place) {
+            public void onPlaceSelected(final Place place) {
+                map.clear();
                 // TODO: Get info about the selected place.
                 Log.i("MapActivity", "Place: " + place.getName() + ", " + place.getLatLng());
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
+                MongoClientURI uri = new MongoClientURI(
+                        "mongodb://cc:cc@cluster0-shard-00-00-zme9k.azure.mongodb.net:27017,cluster0-shard-00-01-zme9k.azure.mongodb.net:27017,cluster0-shard-00-02-zme9k.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&&w=majority");
+
+                final Block<Document> printBlock = new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+                        try {
+                            array.put(new JSONObject(document.toJson()));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                MongoClient mongoClient = new MongoClient(uri);
+                MongoDatabase database = mongoClient.getDatabase("data");
+                final MongoCollection<Document> collection = database.getCollection("locations");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        collection.find().forEach(printBlock);
+                        try {
+                            done(place.getLatLng().latitude < 31);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
             }
 
             @Override
@@ -60,7 +109,41 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
             }
         });
         mapFragment.getMapAsync(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        Log.e("hahahaha", location.toString());
+                        if (location != null) {
+                            final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                        }
+                    }
+                });
+
         return view;
+    }
+
+    private void done(boolean austin) throws JSONException {
+        int start = 0;
+        int end = 5;
+        if(!austin) {
+            start = 5;
+            end = 10;
+        }
+        for(int i = start; i < end; i++) {
+            JSONObject object = (JSONObject) array.get(i);
+            final LatLng latLng = new LatLng((float)object.getDouble("lat"), (float)object.getDouble("long"));
+            Log.e("hahah", latLng.toString());
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    map.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.parking_logo)));
+                }
+            });
+        }
+
     }
 
     @Override
